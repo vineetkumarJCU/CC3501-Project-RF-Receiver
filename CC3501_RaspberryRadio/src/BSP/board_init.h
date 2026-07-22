c270ea3b2c2e36b5979c49a74740c4451ce2abad
Haolin Jiang <haolin.jiang@my.jcu.edu.au>
2026-07-22 10:43:25 +1000

    add test for GPS module

--- CC3501_RaspberryRadio/src/BSP/board_init.h
+++ CC3501_RaspberryRadio/src/BSP/board_init.h
@@ -1,41 +1,30 @@
 #ifndef BOARD_INIT_H
 #define BOARD_INIT_H
 
-#include "board_pin_def.h"
-
-#include "pico/stdlib.h"
-#include "pico/divider.h"
-#include "hardware/spi.h"
-#include "hardware/i2c.h"
-#include "hardware/dma.h"
-#include "hardware/pio.h"
-#include "hardware/interp.h"
-#include "hardware/timer.h"
-#include "hardware/watchdog.h"
 #include "hardware/clocks.h"
-#include "hardware/pll.h"
+#include "hardware/i2c.h"
+#include "hardware/spi.h"
 #include "hardware/uart.h"
-#include "hardware/gpio.h"
-#include "hardware/vreg.h"
-#include "hardware/pwm.h"
-
-
 
 /* SPI Handle for LCD */
-#define ST7789_SPI_HEADER			spi1
+#define ST7789_SPI_HEADER spi1
 /* IIC Handle for SI4732 */
-#define SI4732_IIC_HANDLE     i2c1
+#define SI4732_IIC_HANDLE i2c1
+/* IIC Handle for CST816D */
+#define CST816D_IIC_HANDLE i2c0
+/* SPI Handle for SD card */
+#define SD_CARD_SPI_HEADER spi0
+/* Uart Handle for user communication */
+#define USER_UART_HANDLE uart0
+/* Uart Handle for GPS module communication */
+#define GPS_UART_HANDLE uart1
 
-
-
-#define ST7789_SPI_BAUD_RATE 64*MHZ // 64MHz
-#define SCREEN_BACKLIGHT_PWM_CLK_DIVIDER 256.0f // Fractional divider, format is fix8.4
-#define SCREEN_BACKLIGHT_PWM_WARP 200 // Set PWM frequency to 5kHz with 256MHz clock and 256 divider
-#define USER_UART_BAUD_RATE 2*MHZ // 2Mbps
-#define SI4732_IIC_BAUD_RATE 400*KHZ // 400kHz
-#define CST816D_IIC_BAUD_RATE 400*KHZ // 400kHz
-#define SD_CARD_SPI_BAUD_RATE 16*MHZ // 16MHz
-
+#define ST7789_SPI_BAUD_RATE 120 * MHZ  // 120MHz to avoid SI4732's 64MHz~108MHz FM band
+#define USER_UART_BAUD_RATE 2000000     // 2 Mbps
+#define GPS_UART_BAUD_RATE 9600         // 9600 baud
+#define SI4732_IIC_BAUD_RATE 1000 * KHZ // 1000kHz
+#define CST816D_IIC_BAUD_RATE 400 * KHZ // 400kHz
+#define SD_CARD_SPI_BAUD_RATE 16 * MHZ  // 16MHz
 
 void board_init(void);
 
--- CC3501_RaspberryRadio/src/main/main.c
+++ CC3501_RaspberryRadio/src/main/main.c
@@ -1,21 +1,22 @@
 #include "main.h"
-#include "RP2040_adc.h"
-#include "NS4160.h"
+#include "stdio.h"
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "GPS_module.h"
+
 
 void main(void)
 {
   //test function
   stdio_init_all();
-
-  RP2040_adc_init();
+  GPS_module_powerup();
+  GPS_module_init();
+  gps_info_t s;
+  GPS_module_get_latest_info(&s);
   while (1)
   {
-    float v = RP2040_adc_read_battery_voltage();
-    printf("Battery voltage: %.2f V\n", v);
-
-    bool audio_jack_inserted = RP2040_adc_is_audio_jack_inserted();
-    printf("Audio jack inserted: %s\n", audio_jack_inserted ? "Yes" : "No");
-    
-    sleep_ms(1000);
+    sleep_ms(200);
+    GPS_module_process();
+    GPS_module_log_all_info(&s);
   }
 }
